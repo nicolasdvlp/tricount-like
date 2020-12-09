6640e6640660e8ffeff600e1aca5c50cfccbc801
nicolasdvlp <nicolas.dvlp@gmail.com>
2020-12-09 09:20:43 +0100

    css switch button

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,6 @@
 // == Import npm
 import React from 'react';
-import { Nav  } from 'react-bootstrap';
+import { Nav, Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 // == Import
@@ -12,17 +12,18 @@ import ResultView from '../../containers/Result'
 const Main = ({ switchResultPage, clickChangeView }) => {
     return (
         <main className="main">
-            <Nav variant="pills" className="main__switch" >
-                <Nav.Item>
-                    <Nav.Link  active={!switchResultPage}
-                    onClick={clickChangeView}>Edition</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link active={switchResultPage}
-                    onClick={clickChangeView}>Résultat</Nav.Link>
-                </Nav.Item>
-            </Nav>
-
+            <Container className="justify-content-md-center main__containerswitch">
+                <Nav variant="pills" className="main__switch" >
+                    <Nav.Item>
+                        <Nav.Link  active={!switchResultPage}
+                        onClick={clickChangeView}>Edition</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link active={switchResultPage}
+                        onClick={clickChangeView}>Résultat</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Container>
             {
                 !switchResultPage && <UserList />
             }
