1a04d17584026818190e92b3e1144fa57b82c65d
Nicolas MATEOS <nicolas.dvlp@gmail.com>
2021-06-04 15:38:13 +0200

    add touch gesture init

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 // == Import
 import './App.css';
@@ -10,8 +11,16 @@ import ModalFormExp from '../../containers/ModalFormExp'
 import ModalDelUser from '../../containers/ModalDelUser'
 
 function App() {
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => console.log('slide left'),
+    onSwipedRight: () => console.log('slide right'),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true// ...config,
+  });
+
   return (
-    <div className="App">
+    <div className="App" {...handlers}>
       <Header />
       <Main />
 
